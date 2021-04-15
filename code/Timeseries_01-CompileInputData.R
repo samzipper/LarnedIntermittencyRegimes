e4ee3369566fce9d1c2b5637cf323d645533519d
Sam Zipper <samuelczipper@gmail.com>
2021-04-15 08:34:36 -0500

    Add groundwater pumping, gap-filled LWPH4 water levels to timeseries input data

--- code/Timeseries_01-CompileInputData.R
+++ code/Timeseries_01-CompileInputData.R
@@ -7,17 +7,21 @@ source(file.path("code", "paths+packages.R"))
 df_met <- readr::read_csv(file.path("data", "Meteorology_Daily_WithETo.csv")) %>% 
   dplyr::rename(date_ghcn = date)
 
-df_gw <- readr::read_csv(file.path("data", "Groundwater_WaterLevels-DailyGHCN.csv"))
+df_gw <- readr::read_csv(file.path("data", "Groundwater_WaterLevels-DailyGHCN-LWPH4-GapFill.csv")) %>% 
+  dplyr::select(date_ghcn, LWPH4a, LWPH4a_source, LWPH4b, LWPH4b_source, LWPH4c, LWPH4c_source)
 
 df_stream <- readr::read_csv(file.path("data", "Streamflow+Stage_DailyGHCN.csv"))
+
+df_wuse <- readr::read_csv(file.path("data", "WaterUse_Daily.csv"))
 
 ## combine into single data frame
 df_all <- 
   dplyr::left_join(df_met, df_stream, by = "date_ghcn") %>% 
+  dplyr::left_join(df_wuse, by = "date_ghcn") %>% 
   dplyr::left_join(df_gw, by = "date_ghcn")
 
 ## met data goes back to 1904... start data before earliest streamflow measurement
-yrs_buffer <- 10
+yrs_buffer <- 5
 date_min <- min(df_stream$date_ghcn)
 
 ## trim
