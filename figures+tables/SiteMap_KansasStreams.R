fcb4c7bd5362c17225401e1fdadc96dfdd6eac54
Sam Zipper <samzipper@ku.edu>
2023-03-10 16:42:54 -0600

    Add Kansas stream map with dashed lines

--- figures+tables/SiteMap_KansasStreams.R
+++ figures+tables/SiteMap_KansasStreams.R
@@ -4,7 +4,7 @@ source(file.path("code", "paths+packages.R"))
 
 ## load/prep data
 # where KS GIS data is stored
-dir_GIS <- "C:/Users/samzipper/OneDrive - The University of Kansas/Research/Kansas/GISdata"
+dir_GIS <- "C:/Users/s947z036/OneDrive - University of Kansas/Research/Kansas/GISdata"
 
 # streams
 sf_streams <- 
@@ -46,4 +46,26 @@ ggplot() +
 ggsave(file.path("figures+tables", "SiteMap_KansasStreams.png"),
        width = 190, height = 95, units = "mm")
 ggsave(file.path("figures+tables", "SiteMap_KansasStreams.pdf"),
+       width = 190, height = 95, units = "mm")
+
+## dashed intermittent streams for Jim
+ggplot() +
+  geom_sf(data = sf_HPA, color = NA, fill = "black", alpha = 0.25) +
+  geom_sf(data = sf_streams, aes(color = perennial1961and2009, linetype = perennial1961and2009)) +
+  geom_polygon(data = kansas, aes(x = long, y = lat), color = "black", fill = NA) +
+  scale_color_manual(name = NULL, values = c("FALSE" = col.cat.red, "TRUE" = col.cat.blu),
+                     labels = c("FALSE" = "Perennial in 1961 but not 2009", "TRUE" = "Perennial in 1961 and 2009")) +
+  scale_linetype_manual(name = NULL, values = c("FALSE" = "dotted", "TRUE" = "solid")) +
+  theme_bw() +
+  theme(panel.grid = element_blank(),
+        panel.border = element_blank(),
+        axis.text = element_blank(),
+        axis.title = element_blank(),
+        axis.ticks = element_blank(),
+        plot.margin = margin(t = 0, r = 0, b = 0, l = 0, unit = "pt"),
+        legend.position = "none")
+
+ggsave(file.path("figures+tables", "SiteMap_KansasStreams-Dashed.png"),
+       width = 190, height = 95, units = "mm")
+ggsave(file.path("figures+tables", "SiteMap_KansasStreams-Dashed.pdf"),
        width = 190, height = 95, units = "mm")
